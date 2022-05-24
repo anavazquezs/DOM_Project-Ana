f48182ca3cc170269d319ff9450bef4eeec3d852
Niko Mastakas <nicolasmastakas@gmail.com>
2022-05-23 22:59:08 -0300

    Adding descriptive comments for the task

---
+++ starter/app.js
@@ -0,0 +1,30 @@
+//Contador del jugador uno.
+//Crear un objeto el cual contenta el puntaje del primer jugador
+//Selecionar el boton del jugador uno
+//Selecionar el display del jugador uno
+
+//Contador del jugador dos.
+//Crear un objeto el cual contenta el puntaje del segundo jugador
+//Seleccionar el boton del jugador dos
+//Selecionar el display del jugador dos
+
+//Selecionar el boton de reset
+
+// Selecionar el texto de quien juega
+
+// // Declarar variable de cuantos juegos se haran
+
+// //Declarar booleano para saber si el juego termino o no
+
+//Funcion para actualizar los scores, debe cambiar los colores si el juego termino!
+// 'has-text-success' (Color verde)
+// 'has-text-danger' (Color rojo)
+// Si un score llega al winning score, es game over!
+
+//Poner la funcion update scores a los botones de ambos jugadores!
+
+//Poner la funcion de reset al boto nde reset
+
+// resetButton.addEventListener("click", reset);
+
+// // Funcion para reiniciar todo!
